32b100ef935e71e6d64fb49bd4d25b473d06c90e
Ruslan <ruslanbulah3@gmail.com>
2022-12-24 16:52:33 +0600

    oof

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,19 @@ $(".hamburger").on("click", function () {
     $(".hamburger").toggleClass("is-active");
     $(".nav").toggleClass("is-active");
     $(".overlay").toggleClass("is-active");
+    $("body").toggleClass("fixed");
 });
 
 $(".overlay").on("click", function () {
     $(".hamburger").removeClass("is-active");
     $(".nav").removeClass("is-active");
     $(".overlay").removeClass("is-active");
+    $("body").removeClass("fixed");
+});
+
+const dropbtn = $(".dropbtn");
+const dropbtnIcon = $(".dropbtn-icon");
+
+dropbtn.on("click", function () {
+    $(".dropdown-content").toggleClass("is-active");
 });
